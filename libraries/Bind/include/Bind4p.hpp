dbbad3fcd33cecedeb641c26c4cd4e5cbf148f65
LAMBERT Kevin <lamber_k@localhost.localdomain>
2013-11-12 00:01:18 +0100

    Bind update, no bug detected

--- libraries/Bind/include/Bind4p.hpp
+++ libraries/Bind/include/Bind4p.hpp
@@ -8,9 +8,9 @@
 // Functions //
 ///////////////
 
-template <typename Ret, typename P1=void, typename P2=void, typename P3=void,
-	  typename P4=void>
-class Function
+template <typename Ret, typename P1, typename P2, typename P3,
+	  typename P4>
+class Function<Ret (*)(P1, P2, P3, P4)>
 {
   typedef Ret	(*Func)(P1, P2, P3, P4);
 public:
@@ -122,153 +122,153 @@ protected:
 // Member Functions //
 /////////////////////
 
-// template <typename Ret, class C, typename P1, typename P2, typename P3, typename P4>
-// class Function<Ret (C::*)(P1, P2, P3, P4)>
-// {
-//   typedef	Ret (C::*Func)(P1, P2, P3, P4);
-// public:
-//   Function(Func f, P1 _1, P2 _2, P3 _3, P4 _4) :
-//     _func(f), _inst(0), _1(_1), _2(_2), _3(_3), _4(_4)
-//   {
+template <typename Ret, class C, typename P1, typename P2, typename P3, typename P4>
+class Function<Ret (C::*)(P1, P2, P3, P4)>
+{
+  typedef	Ret (C::*Func)(P1, P2, P3, P4);
+public:
+  Function(Func f, P1 _1, P2 _2, P3 _3, P4 _4) :
+    _func(f), _inst(0), _1(_1), _2(_2), _3(_3), _4(_4)
+  {
 
-//   }
-//   Function(Func f, C *inst, P1 _1, P2 _2, P3 _3, P4 _4) :
-//     _func(f), _inst(inst), _1(_1), _2(_2), _3(_3), _4(_4)
-//   {
+  }
+  Function(Func f, C *inst, P1 _1, P2 _2, P3 _3, P4 _4) :
+    _func(f), _inst(inst), _1(_1), _2(_2), _3(_3), _4(_4)
+  {
 
-//   }
+  }
 
-//   Function(Func f) :
-//     _func(f), _inst(0)
-//   {
+  Function(Func f) :
+    _func(f), _inst(0)
+  {
 
-//   }
-//   Function(Func f, C *inst) :
-//     _func(f), _inst(inst)
-//   {
+  }
+  Function(Func f, C *inst) :
+    _func(f), _inst(inst)
+  {
 
-//   }
+  }
 
-//   virtual ~Function() {};
-//   Ret	operator()(P1 p1, P2 p2, P3 p3, P4 p4)
-//   {
-//     if (_inst != 0)
-//       return ((_inst->*_func)(p1, p2, p3, p4));
-//   }
-//   Ret	operator()(C *inst, P1 p1, P2 p2, P3 p3, P4 p4)
-//   {
-//     return ((inst->*_func)(p1, p2, p3, p4));
-//   }
+  virtual ~Function() {};
+  Ret	operator()(P1 p1, P2 p2, P3 p3, P4 p4)
+  {
+    if (_inst != 0)
+      return ((_inst->*_func)(p1, p2, p3, p4));
+  }
+  Ret	operator()(C *inst, P1 p1, P2 p2, P3 p3, P4 p4)
+  {
+    return ((inst->*_func)(p1, p2, p3, p4));
+  }
 
-//   Ret	operator()()
-//   {
-//     if (_inst != 0)
-//       return ((_inst->*_func)(_1, _2, _3, _4));
-//   }
-//   Ret	operator()(C *inst)
-//   {
-//     return ((inst->*_func)(_1, _2, _3, _4));
-//   }
+  Ret	operator()()
+  {
+    if (_inst != 0)
+      return ((_inst->*_func)(_1, _2, _3, _4));
+  }
+  Ret	operator()(C *inst)
+  {
+    return ((inst->*_func)(_1, _2, _3, _4));
+  }
 
-//   Function(const Function &src) : _func(src._func), _inst(src._inst),
-// 				  _1(src._1), _2(src._2),
-// 				  _3(src._3), _4(src._4) {}
-//   Function	&operator=(const Function &src)
-//   {
-//     if (&src != this)
-//       {
-// 	_func = src._func;
-// 	_inst = src._inst;
-// 	_1 = src._1;
-// 	_2 = src._2;
-// 	_3 = src._3;
-// 	_4 = src._4;
-//       }
-//     return (*this);
-//   }
+  Function(const Function &src) : _func(src._func), _inst(src._inst),
+				  _1(src._1), _2(src._2),
+				  _3(src._3), _4(src._4) {}
+  Function	&operator=(const Function &src)
+  {
+    if (&src != this)
+      {
+	_func = src._func;
+	_inst = src._inst;
+	_1 = src._1;
+	_2 = src._2;
+	_3 = src._3;
+	_4 = src._4;
+      }
+    return (*this);
+  }
 
-// protected:
-//   Func	_func;
-//   C	*_inst;
-//   P1	_1;
-//   P2	_2;
-//   P3	_3;
-//   P4	_4;
-// };
+protected:
+  Func	_func;
+  C	*_inst;
+  P1	_1;
+  P2	_2;
+  P3	_3;
+  P4	_4;
+};
 
-// template <class C, typename P1, typename P2, typename P3, typename P4>
-// class Function<void (C::*)(P1, P2, P3, P4)>
-// {
-//   typedef	void (C::*Func)(P1, P2, P3, P4);
-// public:
-//   Function(Func f, P1 _1, P2 _2, P3 _3, P4 _4) :
-//     _func(f), _inst(0), _1(_1), _2(_2), _3(_3), _4(_4)
-//   {
+template <class C, typename P1, typename P2, typename P3, typename P4>
+class Function<void (C::*)(P1, P2, P3, P4)>
+{
+  typedef	void (C::*Func)(P1, P2, P3, P4);
+public:
+  Function(Func f, P1 _1, P2 _2, P3 _3, P4 _4) :
+    _func(f), _inst(0), _1(_1), _2(_2), _3(_3), _4(_4)
+  {
 
-//   }
+  }
 
-//   Function(Func f, C *inst, P1 _1, P2 _2, P3 _3, P4 _4) :
-//     _func(f), _inst(inst), _1(_1), _2(_2), _3(_3), _4(_4)
-//   {
+  Function(Func f, C *inst, P1 _1, P2 _2, P3 _3, P4 _4) :
+    _func(f), _inst(inst), _1(_1), _2(_2), _3(_3), _4(_4)
+  {
 
-//   }
+  }
 
-//   Function(Func f) :
-//     _func(f), _inst(0)
-//   {
+  Function(Func f) :
+    _func(f), _inst(0)
+  {
 
-//   }
+  }
 
-//   Function(Func f, C *inst) :
-//     _func(f), _inst(inst)
-//   {
+  Function(Func f, C *inst) :
+    _func(f), _inst(inst)
+  {
 
-//   }
+  }
 
-//   virtual ~Function() {};
-//   void	operator()()
-//   {
-//     if (_inst != 0)
-//       (_inst->*_func)(_1, _2, _3, _4);
-//   }
-//   void	operator()(C *inst)
-//   {
-//     (inst->*_func)(_1, _2, _3, _4);
-//   }
+  virtual ~Function() {};
+  void	operator()()
+  {
+    if (_inst != 0)
+      (_inst->*_func)(_1, _2, _3, _4);
+  }
+  void	operator()(C *inst)
+  {
+    (inst->*_func)(_1, _2, _3, _4);
+  }
 
-//   void	operator()(P1 p1, P2 p2, P3 p3, P4 p4)
-//   {
-//     if (_inst != 0)
-//       (_inst->*_func)(p1, p2, p3, p4);
-//   }
-//   void	operator()(C *inst, P1 p1, P2 p2, P3 p3, P4 p4)
-//   {
-//     (inst->*_func)(p1, p2, p3, p4);
-//   }
-//   Function(const Function &src) : _func(src._func), _inst(src._inst),
-// 				  _1(src._1), _2(src._2),
-// 				  _3(src._3), _4(src._4) {}
-//   Function	&operator=(const Function &src)
-//   {
-//     if (&src != this)
-//       {
-// 	_func = src._func;
-// 	_inst = src._inst;
-// 	_1 = src._1;
-// 	_2 = src._2;
-// 	_3 = src._3;
-// 	_4 = src._4;
-//       }
-//     return (*this);
-//   }
+  void	operator()(P1 p1, P2 p2, P3 p3, P4 p4)
+  {
+    if (_inst != 0)
+      (_inst->*_func)(p1, p2, p3, p4);
+  }
+  void	operator()(C *inst, P1 p1, P2 p2, P3 p3, P4 p4)
+  {
+    (inst->*_func)(p1, p2, p3, p4);
+  }
+  Function(const Function &src) : _func(src._func), _inst(src._inst),
+				  _1(src._1), _2(src._2),
+				  _3(src._3), _4(src._4) {}
+  Function	&operator=(const Function &src)
+  {
+    if (&src != this)
+      {
+	_func = src._func;
+	_inst = src._inst;
+	_1 = src._1;
+	_2 = src._2;
+	_3 = src._3;
+	_4 = src._4;
+      }
+    return (*this);
+  }
 
-// protected:
-//   Func	_func;
-//   C	*_inst;
-//   P1	_1;
-//   P2	_2;
-//   P3	_3;
-//   P4	_4;
-// };
+protected:
+  Func	_func;
+  C	*_inst;
+  P1	_1;
+  P2	_2;
+  P3	_3;
+  P4	_4;
+};
 
 #endif /* BIND1P_H_ */
--- libraries/Bind/test/main.cpp
+++ libraries/Bind/test/main.cpp
@@ -61,6 +61,51 @@ int	_fooRet1p(int a)
   return (r);
 }
 
+void	_foo2p(int a, double b)
+{
+  std::cout << __PRETTY_FUNCTION__ << std::endl;
+  std::cout << a << ":" << b << std::endl;
+}
+
+int	_fooRet2p(int a, double b)
+{
+  int	r = std::rand();
+
+  std::cout << __PRETTY_FUNCTION__ << ":" << r << std::endl;
+  std::cout << a << ":" << b << std::endl;
+  return (r);
+}
+
+void	_foo3p(int a, double b, char c)
+{
+  std::cout << __PRETTY_FUNCTION__ << std::endl;
+  std::cout << a << ":" << b << ":" << c << std::endl;
+}
+
+int	_fooRet3p(int a, double b, char c)
+{
+  int	r = std::rand();
+
+  std::cout << __PRETTY_FUNCTION__ << ":" << r << std::endl;
+  std::cout << a << ":" << b << ":" << c << std::endl;
+  return (r);
+}
+
+void	_foo4p(int a, double b, char c, std::string *s)
+{
+  std::cout << __PRETTY_FUNCTION__ << std::endl;
+  std::cout << a << ":" << b << ":" << c << ":" << *s << std::endl;
+}
+
+int	_fooRet4p(int a, double b, char c, std::string *s)
+{
+  int	r = std::rand();
+
+  std::cout << __PRETTY_FUNCTION__ << ":" << r << std::endl;
+  std::cout << a << ":" << b << ":" << c << ":" << *s << std::endl;
+  return (r);
+}
+
 int					main()
 {
   A							inst(10);
@@ -70,26 +115,42 @@ int					main()
   Function<int (*)()>					fooRet = Func::Bind(&::_fooRet);
   Function<void (*)(int)>				foo1p = Func::Bind(&::_foo1p, 10);
   Function<int (*)(int)>				fooRet1p = Func::Bind(&::_fooRet1p, 20);
+  Function<void (*)(int, double)>			foo2p = Func::Bind(&::_foo2p, 10, 3.4);
+  Function<int (*)(int, double)>			fooRet2p = Func::Bind(&::_fooRet2p, 20, 4.2);
+  Function<void (*)(int, double, char)>			foo3p = Func::Bind(&::_foo3p, 10,
+									   3.5, 'a');
+  Function<int (*)(int, double, char)>			fooRet3p = Func::Bind(&::_fooRet3p, 20,
+									      5.3, 'b');
+  Function<void (*)(int, double, char, std::string *)>	foo4p = Func::Bind(&::_foo4p, 10,
+									   4.5, 'a', &s);
+  Function<int (*)(int, double, char, std::string *)>	fooRet4p = Func::Bind(&::_fooRet4p, 20,
+									      5.3, 'b', &s);
   Function<int (A::*)()>				fooClass = Func::Bind(&A::run, &inst);
   Function<int (A::*)()>				fooClass2 = Func::Bind(&A::run);
   Function<int (A::*)(int)>				fooClass1p = Func::Bind(&A::foo, &inst, 12);
   Function<int (A::*)(int)>				fooClass1p2 = Func::Bind(&A::foo, 23);
   Function<int (A::*)(int, double)>			fooClass2p = Func::Bind(&A::foo, &inst, 12,
-										3.4);
+  										3.4);
   Function<int (A::*)(int, double)>			fooClass2p2 = Func::Bind(&A::foo, 23, 4.2);
   Function<int (A::*)(int, double, char)>		fooClass3p = Func::Bind(&A::foo, &inst, 12,
-										3.4, 'a');
+  										3.4, 'a');
   Function<int (A::*)(int, double, char)>		fooClass3p2 = Func::Bind(&A::foo, 23, 4.2,
-										 'b');
+  										 'b');
   Function<int (A::*)(int, double, char, std::string *)> fooClass4p = Func::Bind(&A::bar, &inst,
-										 12, 3.4, 'a', &s);
+  										 12, 3.4, 'a', &s);
   Function<int (A::*)(int, double, char, std::string *)> fooClass4p2 = Func::Bind(&A::bar, 23, 4.2,
-										  'b', &s);
+  										  'b', &s);
 
   foo();
   std::cout << "Retval: " << fooRet() << std::endl;
   foo1p();
   std::cout << "Retval: " << fooRet1p() << std::endl;
+  foo2p();
+  std::cout << "Retval: " << fooRet2p() << std::endl;
+  foo3p();
+  std::cout << "Retval: " << fooRet3p() << std::endl;
+  foo4p();
+  std::cout << "Retval: " << fooRet4p() << std::endl;
   fooClass();
   fooClass2(&inst2);
   fooClass1p();
