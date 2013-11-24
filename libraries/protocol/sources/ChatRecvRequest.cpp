d3adf7c476e151ccf7a7779c59304c3ad1828b50
lemart-m <maxime.lemartret@epitech.eu>
2013-11-24 14:08:14 +0100

    ChatRecvRequest

---
+++ libraries/protocol/sources/ChatRecvRequest.cpp
@@ -0,0 +1,55 @@
+#include "ChatRecvRequest.h"
+#include "Protocol.hpp"
+
+ChatRecvRequest::ChatRecvRequest(const std::string &msg) : ARequest(requestCode::chat::RECV_MSG)
+{
+	_msg = msg;
+}
+
+ChatRecvRequest::~ChatRecvRequest()
+{
+}
+
+ChatRecvRequest::ChatRecvRequest(ChatRecvRequest const& src) : ARequest(requestCode::chat::RECV_MSG)
+{
+	_msg = src._msg;
+}
+
+ChatRecvRequest& ChatRecvRequest::operator=(ChatRecvRequest const& src)
+{
+	if (&src != this)
+	{
+		_code = src._code;
+		_msg = src._msg;
+	}
+	return (*this);
+}
+
+std::string ChatRecvRequest::msg() const
+{
+	return _msg;
+}
+
+
+Protocol	&ChatRecvRequest::serialize(Protocol &rhs) const
+{
+	Ruint16	len = _msg.length();
+
+	rhs << _code << len;
+	rhs.push(_msg, len);
+	return (rhs);
+}
+
+Protocol	&ChatRecvRequest::unserialize(Protocol &rhs)
+{
+	Ruint16	len;
+
+	rhs >> len;
+	rhs.pop(_msg, len);
+	return (rhs);
+}
+
+ARequest	*ChatRecvRequest::clone()
+{
+	return (new ChatRecvRequest(_msg));
+}
