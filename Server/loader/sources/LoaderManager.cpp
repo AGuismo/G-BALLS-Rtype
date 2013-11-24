39275b4c97e9fe4291fdc2e148d24e8abf3cadf5
AGuismo <simon.spozio@gmail.com>
2013-11-24 17:25:00 +0100

    update loader

--- Server/loader/sources/LoaderManager.cpp
+++ Server/loader/sources/LoaderManager.cpp
@@ -21,7 +21,7 @@ namespace	botLoader
 
 	void	Manager::initialize(std::string &dName)
 	{
-		std::cout << "Bot loading file: " << dName.c_str() << std::endl;
+//		std::cout << "Bot loading file: " << dName.c_str() << std::endl;
 		_checkFile = new checkFileAbstract(dName);
 		_dynLoader = new DynamicAbstract;
 		_upList = _checkFile->refreshFile();
@@ -30,10 +30,10 @@ namespace	botLoader
 
 		for (std::map<std::string, UPDATE>::iterator it = _upList->begin(); it != _upList->end(); ++it)
 		{
-			std::cout << "[" << it->first.c_str() << "] [" << it->second << "]" << std::endl;
+//			std::cout << "[" << it->first.c_str() << "] [" << it->second << "]" << std::endl;
 			if (it->second == NEW)
 				addNewIa("./botlibrary/" + it->first);
-			std::cout << std::endl;
+//			std::cout << std::endl;
 		}
 	}
 
@@ -51,53 +51,100 @@ namespace	botLoader
 			instance_call	inst;
 
 			inst = (instance_call)_dynLoader->DynamicLoadSym(std::string("getInstance"));
-			if (inst != NULL)
+			if (inst != 0)
 			{
 				AIaAlgo	*ia = inst();
 
 				if (ia->isIa())
 				{
-					std::cout << "ok" << std::endl;
+//					std::cout << "ok ia" << std::endl;
+					_simpleBydos.insert(std::pair<std::string, AIaAlgo *>(path, ia));
+				}
+				else
+				{
+//					std::cout << "ok boss" << std::endl;
+					_bossBydos.insert(std::pair<std::string, AIaAlgo *>(path, ia));
+				}
+				return true;
+			}
+/*			else
+				std::cout << "open not working: " << path.c_str() << std::endl;*/
+		}
+		return false;
+	}
+
+	bool	Manager::updateIa(const std::string &path)
+	{
+		if (_dynLoader->DynamicOpen(path) == true)
+		{
+			instance_call	inst;
+
+			inst = (instance_call)_dynLoader->DynamicLoadSym(std::string("getInstance"));
+			if (inst != 0)
+			{
+				AIaAlgo	*ia = inst();
+
+				if (ia->isIa())
+				{
+//					std::cout << "ok ia" << std::endl;
 					_simpleBydos[path] = ia;
 				}
 				else
+				{
+//					std::cout << "ok boss" << std::endl;
 					_bossBydos[path] = ia;
-
+				}
+				return true;
 			}
-			else
-				std::cout << "open not working: " << path.c_str() << std::endl;
+/*			else
+				std::cout << "open not working: " << path.c_str() << std::endl;*/
 		}
 		return false;
 	}
 
 	void	Manager::routine(Manager *man)
 	{
-	  (void)man;
-	  /*
-		    while (true)
-			  {
-			  sys::sleep(8);
-
+		  (void)man;
+		  while (true)
+		  {
+			  sys::sleep(10);
 			  man->_upList = man->_checkFile->refreshFile();
 			  if (man->_upList != NULL)
-			  {
-			  #ifdef DEBUG
-			  for (std::map<std::string, UPDATE>::iterator it = man->_upList->begin(); it != man->_upList->end(); ++it)
-			  {
-			  std::cout << "[" << it->first.c_str() << "] [" << it->second << "]" << std::endl;
-			  //man->addNewIa("./botlibrary/" + it->first);
-			  }
-			  std::cout << std::endl;
-			  #endif
-			  }
-			  }*/
+				  {
+					  for (std::map<std::string, UPDATE>::iterator it = man->_upList->begin(); it != man->_upList->end(); ++it)
+					  {
+	//					  std::cout << "[" << it->first.c_str() << "] [" << it->second << "]" << std::endl;
+						  if (it->second == NEW)
+						  {
+//							  std::cout << "new IA" << it->first.c_str() << std::endl;
+							  man->addNewIa("./botlibrary/" + it->first);
+						  }
+						  else if (it->second == UPDATED)
+						  {
+//							 std::cout << "Update: " << it->first.c_str() << std::endl;
+							 man->addNewIa("./botlibrary/" + it->first);
+						  }
+					  }
+//					  std::cout << std::endl;
+				  }
+			}
 	}
+}
+
+/*AIaAlgo									*Manager::getSimpleBydos(void)
+{
+	return NULL;
+}*/
+
+
+
+
+/*AIaAlgo									*Manager::getSimpleBydos(void)
+{
 
 }
-/*enum UPDATE
-  {
-  NEW = 1,
-  UPDATED,
-  UNCHANGED,
-  DELETED
-  };*/
+
+AIaAlgo									*Manager::getBossBydos(void)
+{
+
+}*/
