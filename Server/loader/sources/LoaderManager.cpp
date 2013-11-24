1713a2d793433fc8a68d251e47c1a07154ccf2e3
AGuismo <simon.spozio@gmail.com>
2013-11-24 15:41:28 +0100

    no more c....

--- Server/loader/sources/LoaderManager.cpp
+++ Server/loader/sources/LoaderManager.cpp
@@ -8,92 +8,90 @@
 
 namespace	botLoader
 {
-  Manager::Manager():
-    _th(Func::Bind(&Manager::routine, this))
-  {
+	Manager::Manager() :
+		_th(Func::Bind(&Manager::routine, this))
+	{
 
-  }
+	}
 
-  Manager::~Manager()
-  {
-    _th.cancel();
-  }
+	Manager::~Manager()
+	{
+		_th.cancel();
+	}
 
-  void	Manager::initialize(std::string &dName)
-  {
-#ifdef DEBUG
-    std::cout << "Bot loading file: " << dName.c_str() << std::endl;
-#endif
-    _checkFile = new checkFileAbstract(dName);
-    _dynLoader = new DynamicAbstract;
-    _upList = _checkFile->refreshFile();
-    if (_upList == NULL)
-      throw Exception("Bot library repertory empty or inexistant");
+	void	Manager::initialize(std::string &dName)
+	{
+		std::cout << "Bot loading file: " << dName.c_str() << std::endl;
+		_checkFile = new checkFileAbstract(dName);
+		_dynLoader = new DynamicAbstract;
+		_upList = _checkFile->refreshFile();
+		if (_upList == NULL)
+			throw Exception("Bot library repertory empty or inexistant");
 
-    for (std::map<std::string, UPDATE>::iterator it = _upList->begin(); it != _upList->end(); ++it)
-      {
-	std::cout << "[" << it->first.c_str() << "] [" << it->second << "]" << std::endl;
-	if (it->second == NEW)
-	  {
-	    addNewIa("./botlibrary/" + it->first);
-	  }
-	std::cout << std::endl;
-      }
-  }
+		for (std::map<std::string, UPDATE>::iterator it = _upList->begin(); it != _upList->end(); ++it)
+		{
+			std::cout << "[" << it->first.c_str() << "] [" << it->second << "]" << std::endl;
+			if (it->second == NEW)
+				addNewIa("./botlibrary/" + it->first);
+			std::cout << std::endl;
+		}
+	}
 
-  void	Manager::run()
-  {
-    _th.run();
-    std::cout << "Loader manager started..." << std::endl;
-  }
+	void	Manager::run()
+	{
+		_th.run();
+		std::cout << "Loader manager started..." << std::endl;
+	}
 
-  bool	Manager::addNewIa(const std::string &path)
-  {
+	bool	Manager::addNewIa(const std::string &path)
+	{
 
-    if (_dynLoader->DynamicOpen(path) == true)
-      {
-	instance_call	inst;
+		if (_dynLoader->DynamicOpen(path) == true)
+		{
+			instance_call	inst;
 
-	inst = (instance_call)_dynLoader->DynamicLoadSym(std::string("getInstance"));
-	if (inst != 0)
-	  {
-	    AIaAlgo	*ia = inst();
+			inst = (instance_call)_dynLoader->DynamicLoadSym(std::string("getInstance"));
+			if (inst != NULL)
+			{
+				AIaAlgo	*ia = inst();
 
-	    if (ia->isIa())
-	      _simpleBydos[path] = ia;
-	    else
-	      _bossBydos[path] = ia;
-	  }
-      }
-    else
-      std::cout << "open not working: " << path.c_str() << std::endl;
-    return false;
-  }
+				if (ia->isIa())
+				{
+					std::cout << "ok" << std::endl;
+					_simpleBydos[path] = ia;
+				}
+				else
+					_bossBydos[path] = ia;
 
-  void	Manager::routine(Manager *man)
-  {
-    while (true)
-      {
-	sys::sleep(8);
+			}
+			else
+				std::cout << "open not working: " << path.c_str() << std::endl;
+		}
+		return false;
+	}
 
-	man->_upList = man->_checkFile->refreshFile();
-	if (man->_upList != NULL)
-	  {
-#ifdef DEBUG
-	    for (std::map<std::string, UPDATE>::iterator it = man->_upList->begin(); it != man->_upList->end(); ++it)
-	      {
-		std::cout << "[" << it->first.c_str() << "] [" << it->second << "]" << std::endl;
-		man->addNewIa("./botlibrary/" + it->first);
-		std::cout << "caca" << std::endl;
-	      }
-	    std::cout << std::endl;
-#endif
-	  }
-      }
-  }
+	void	Manager::routine(Manager *man)
+	{
+		/*    while (true)
+			  {
+			  sys::sleep(8);
+
+			  man->_upList = man->_checkFile->refreshFile();
+			  if (man->_upList != NULL)
+			  {
+			  #ifdef DEBUG
+			  for (std::map<std::string, UPDATE>::iterator it = man->_upList->begin(); it != man->_upList->end(); ++it)
+			  {
+			  std::cout << "[" << it->first.c_str() << "] [" << it->second << "]" << std::endl;
+			  //man->addNewIa("./botlibrary/" + it->first);
+			  }
+			  std::cout << std::endl;
+			  #endif
+			  }
+			  }*/
+	}
+
 }
-
-
 /*enum UPDATE
   {
   NEW = 1,
