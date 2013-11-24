6dc5c89e873382bebd1e49b5f93c6ffe88d096dc
lemart-m <maxime.lemartret@epitech.eu>
2013-11-24 14:02:52 +0100

    chatSendRequest++

---
+++ libraries/protocol/sources/ChatSendRequest.cpp
@@ -0,0 +1,55 @@
+#include "ChatSendRequest.h"
+#include "Protocol.hpp"
+
+ChatSendRequest::ChatSendRequest(const std::string &msg) : ARequest(requestCode::chat::SEND_MSG)
+{
+	_msg = msg;
+}
+
+ChatSendRequest::~ChatSendRequest()
+{
+}
+
+ChatSendRequest::ChatSendRequest(ChatSendRequest const& src) : ARequest(requestCode::chat::SEND_MSG)
+{
+	_msg = src._msg;
+}
+
+ChatSendRequest& ChatSendRequest::operator=(ChatSendRequest const& src)
+{
+	if (&src != this)
+	{
+		_code = src._code;
+		_msg = src._msg;
+	}
+	return (*this);
+}
+
+std::string ChatSendRequest::msg() const
+{
+	return _msg;
+}
+
+
+Protocol	&ChatSendRequest::serialize(Protocol &rhs) const
+{
+	Ruint16	len = _msg.length();
+
+	rhs << _code << len;
+	rhs.push(_msg, len);
+	return (rhs);
+}
+
+Protocol	&ChatSendRequest::unserialize(Protocol &rhs)
+{
+	Ruint16	len;
+
+	rhs >> len;
+	rhs.pop(_msg, len);
+	return (rhs);
+}
+
+ARequest	*ChatSendRequest::clone()
+{
+	return (new ChatSendRequest(_msg));
+}
